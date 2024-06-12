a6e03af108de962f5ccf8ab753602c4e7c8abbad
sinan urgun <77778835+SinanUrgunWork@users.noreply.github.com>
2024-06-12 13:54:31 +0300

    Update backend.tf

--- root/backend.tf
+++ root/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-2-tier-project-haplorn"
+    bucket = "****"
     key = "backend/2-tier-app.tf.state"
     region = "eu-central-1"
     dynamodb_table = "remote-backend"
